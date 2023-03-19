1e2b0a3e3ae4163e203e46e159b9fcb4ba455ab5
StevenARB <92124639+StevenARB@users.noreply.github.com>
2023-03-18 19:29:00 -0600

    Initial Commit

---
+++ src/main/java/Proyecto/StaffGymApplication.java
@@ -0,0 +1,13 @@
+package Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffGymApplication.class, args);
+	}
+
+}
