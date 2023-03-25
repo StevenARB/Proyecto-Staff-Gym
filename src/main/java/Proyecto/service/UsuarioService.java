c86a4bf5f24ada67a127859d91adcc83dbc6be1b
StevenARB <92124639+StevenARB@users.noreply.github.com>
2023-03-25 15:52:09 -0600

    Usuario MVC - Tabla de Usuarios

---
+++ src/main/java/Proyecto/service/UsuarioService.java
@@ -0,0 +1,39 @@
+package Proyecto.service;
+
+import Proyecto.entity.Usuario;
+import Proyecto.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Steven
+ */
+@Service
+public class UsuarioService implements IUsuarioService {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public List<Usuario> getAllUsuario() {
+        return (List<Usuario>)usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario getUsuarioById(long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void saveUsuario(Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void delete(long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+}
