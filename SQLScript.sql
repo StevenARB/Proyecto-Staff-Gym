d3206f41c949940112c7c2c7536914596c788870
Aaron Bustos <105888522+AJBustos28@users.noreply.github.com>
2023-04-10 19:31:43 -0600

    Update SQLScript.sql

--- SQLScript.sql
+++ SQLScript.sql
@@ -13,9 +13,9 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `usuarios` VALUES (1,'Luis','Guerrero','Mora','1-9880-7590','luisgm98@gmail.com','8989-9090', '10/03/2023');
-INSERT INTO `usuarios` VALUES (2,'Bryan','Mora','Quesada','1-6425-3637','brmoque123@gmail.com','7478-5678', '10/03/2023');
-INSERT INTO `usuarios` VALUES (3,'María','Flores','Miranda','1-8612-8852','marflomi32@gmail.com','2565-0925', '10/03/2023');
+INSERT INTO `usuarios` VALUES (1,'Luis','Guerrero','Mora','1-9880-7590','luisgm98@gmail.com','8989-9090', '10/03/2023','$2a$12$yPcO1JXl.hyWWcx7lbLkjOjnFdYJrvupKczw2Lyj7G1jn5nCTo1Xe','ADMIN','ADMIN',1);
+INSERT INTO `usuarios` VALUES (2,'Bryan','Mora','Quesada','1-6425-3637','brmoque123@gmail.com','7478-5678', '10/03/2023','$2a$12$r5B.uvQsc15.aT/7lXmC0OPNB2SyxXa1a0SQSWHagBj8IJFkotfyO','USER','USER',1);
+INSERT INTO `usuarios` VALUES (3,'María','Flores','Miranda','1-8612-8852','marflomi32@gmail.com','2565-0925', '10/03/2023'.'$2a$12$5d99ilZwUsDxKcLzaNBbLuN11PG/PdaJLhqHOD6HgxeaMo5FfY9wS','ADMIN','ADMIN',1);
 
 ALTER TABLE `usuarios`(
   `password` varchar(255) NOT NULL,
