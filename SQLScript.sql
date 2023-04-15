e035af56bb66968d679ce8ddb96809f94b29ecd1
StevenARB <92124639+StevenARB@users.noreply.github.com>
2023-04-15 01:42:42 -0600

    SQLScript Usuario Modificado y Nueva Tabla Cliente

--- SQLScript.sql
+++ SQLScript.sql
@@ -4,23 +4,38 @@ use staffgym;
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
-  `apellido1` varchar(45) NOT NULL,
-  `apellido2` varchar(45) NOT NULL,
-  `cedula` varchar(11) NOT NULL,
-  `email` varchar(75) DEFAULT NULL,
-  `telefono` varchar(9) NOT NULL,
-  `fecha` varchar(10) NOT NULL,
-  
+  `email` varchar(75) NOT NULL,
   `password` varchar(255) NOT NULL,
   `permissions` varchar(45) NOT NULL,
   `roles` varchar(45) NOT NULL,
+  `fecha` varchar(10) NOT NULL,
   `active` INT NOT NULL,
+  `clientes_id` INT(11),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_personas_clientes` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+INSERT INTO `usuarios` VALUES (1,'Luis','luisgm98@gmail.com','$2a$12$yPcO1JXl.hyWWcx7lbLkjOjnFdYJrvupKczw2Lyj7G1jn5nCTo1Xe','ADMIN','ADMIN', '10/03/2023', 1, null);
+INSERT INTO `usuarios` VALUES (2,'Bryan', 'brmoque123@gmail.com', '$2a$12$r5B.uvQsc15.aT/7lXmC0OPNB2SyxXa1a0SQSWHagBj8IJFkotfyO','USER','USER','10/03/2023', 1, null);
+INSERT INTO `usuarios` VALUES (3,'María','marflomi32@gmail.com', '$2a$12$5d99ilZwUsDxKcLzaNBbLuN11PG/PdaJLhqHOD6HgxeaMo5FfY9wS','ADMIN','ADMIN', '10/03/2023', 1, null);
+
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido1` varchar(45) NOT NULL,
+  `apellido2` varchar(45) NOT NULL,
+  `cedula` varchar(11) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
+  `fecha` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `usuarios` VALUES (1,'Luis','Guerrero','Mora','1-9880-7590','luisgm98@gmail.com','8989-9090', '10/03/2023','$2a$12$yPcO1JXl.hyWWcx7lbLkjOjnFdYJrvupKczw2Lyj7G1jn5nCTo1Xe','ADMIN','ADMIN',1);
-INSERT INTO `usuarios` VALUES (2,'Bryan','Mora','Quesada','1-6425-3637','brmoque123@gmail.com','7478-5678', '10/03/2023','$2a$12$r5B.uvQsc15.aT/7lXmC0OPNB2SyxXa1a0SQSWHagBj8IJFkotfyO','USER','USER',1);
-INSERT INTO `usuarios` VALUES (3,'María','Flores','Miranda','1-8612-8852','marflomi32@gmail.com','2565-0925', '10/03/2023'.'$2a$12$5d99ilZwUsDxKcLzaNBbLuN11PG/PdaJLhqHOD6HgxeaMo5FfY9wS','ADMIN','ADMIN',1);
+INSERT INTO `clientes` VALUES (1,'Luis','Guerrero','Mora','1-9880-7590','luisgm98@gmail.com','8989-9090', '10/03/2023');
+INSERT INTO `clientes` VALUES (2,'Bryan','Mora','Quesada','1-6425-3637','brmoque123@gmail.com','7478-5678', '10/03/2023');
+INSERT INTO `clientes` VALUES (3,'María','Flores','Miranda','1-8612-8852','marflomi32@gmail.com','2565-0925', '10/03/2023');
+
 
 CREATE TABLE `anuncios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -30,6 +45,7 @@ CREATE TABLE `anuncios` (
   `imagen` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
 
 CREATE TABLE `contacto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -41,7 +57,6 @@ CREATE TABLE `contacto` (
   `email` varchar(75) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
-
 
 INSERT INTO `contacto` VALUES (1,'Luis','Guerrero Mora','San Jose','Del barrio chino sube ','8989-9090','luisgm98@gmail.com');
 INSERT INTO `contacto` VALUES (2,'Jorge','Mora Artavia','Cartago', 'De la esquina subiendo','7478-5678','brmoque123@gmail.com');
@@ -64,6 +79,7 @@ INSERT INTO `reservacion` VALUES (1,'Luis','Guerrero','Mora','1-9880-7590','luis
 INSERT INTO `reservacion` VALUES (2,'Saul','Mora','Quesada','3-0309-780','aaron1@gmail.com','7478-5678', '14/03/2023');
 INSERT INTO `reservacion` VALUES (3,'Marlene','Molina','Rodriguez','3-0306-815','marmolina@gmail.com','8698-5728', '02/03/2023');
 
+
 CREATE TABLE `foro` (
 `id` int(20) NOT NULL AUTO_INCREMENT,
 `autor` varchar(50) NOT NULL,
@@ -71,6 +87,7 @@ CREATE TABLE `foro` (
 `mensaje` varchar (500) NOT NULL,
 PRIMARY KEY (`id`)
 );
+
 
  CREATE TABLE `productos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
